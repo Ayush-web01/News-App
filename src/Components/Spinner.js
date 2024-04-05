2f34fe272fa41a451890c2a15c4cca4087b5e42d
Ayush-web01 <ayushgupta.web01.com>
2024-04-05 12:41:17 +0530

    initialize using create react app

---
+++ src/Components/Spinner.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Spinner = ()=> {
+        return (<div className="text-center">
+            <div className="spinner-grow " role="status">
+            <span className="sr-only">Loading...</span>
+          </div>
+          </div>
+        )
+}
+
+export default Spinner
